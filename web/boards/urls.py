4a99a747e84df5bdee9ecc6d1bb3931ee133c846
leapwing <leapwing@gmail.com>
2017-10-27 15:47:45 +0800

    Advanced Concepts

--- web/boards/urls.py
+++ web/boards/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^hello$', views.hello, name='hello'),
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
